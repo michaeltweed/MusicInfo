4470bca9023af52b7832e0f10076868232f6429d
michaeltweed <michaeltweed23@gmail.com>
2014-12-07 11:22:39 +0000

    Add Constants file

---
+++ app/src/main/java/com/michaeltweed/android/musicinfo/Constants.java
@@ -0,0 +1,7 @@
+package com.michaeltweed.android.musicinfo;
+
+public class Constants {
+    /* This class contains important information such as API keys
+       and any other required credentials. You will have to get these for yourself :)
+     */
+}
