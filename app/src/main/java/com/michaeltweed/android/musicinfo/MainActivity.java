6931d90a43d38105782e5b15749be3e31a54e496
michaeltweed <michaeltweed23@gmail.com>
2015-01-20 22:55:55 +0000

    Formatting

--- app/src/main/java/com/michaeltweed/android/musicinfo/MainActivity.java
+++ app/src/main/java/com/michaeltweed/android/musicinfo/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends FragmentActivity {
             }
         });
 
-
         findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
